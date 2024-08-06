727afe06f958a28da05c78a634218a7fbf74f6f6
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-06 11:25:47 +0530

    Add some CSS to the footer to checkout the position of the web page

--- reactweb/src/components/FooterPage.jsx
+++ reactweb/src/components/FooterPage.jsx
@@ -3,7 +3,11 @@ import "./FooterPage.css";
 function FooterPage(){
     return(
         <>
-
+        <div className="footermain">
+            <div className="footerleft"></div>
+            <div className="footercenter"></div>
+            <div className="footerright"></div>
+        </div>
         </>
     )
 }
