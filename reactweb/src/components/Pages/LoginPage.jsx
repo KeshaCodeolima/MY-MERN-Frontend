45320dc217945b3af8cc117722eaf0b2e6b052b3
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-11 11:49:54 +0530

    create the login form validation usestates

--- reactweb/src/components/Pages/LoginPage.jsx
+++ reactweb/src/components/Pages/LoginPage.jsx
@@ -8,6 +8,7 @@ function LoginPage() {
 
   const [email, setEmail]= useState('');
   const [password, setPassword]= useState('');
+  const [errors, setErrors] = useState('');
   const navigate = useNavigate();
 
 
