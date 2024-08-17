646afbf24915db186b4fd357d483ee9d47170b1e
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-17 11:54:40 +0530

    Add the conact page left side iteams

--- reactweb/src/components/Pages/ContactPage.jsx
+++ reactweb/src/components/Pages/ContactPage.jsx
@@ -3,7 +3,19 @@ import React from "react";
 function ContactPage (){
     return(
         <>
-        
+        <div className="main1">
+            <div className="mainleft">
+                <div className="leftiteams">
+                    <li>Location</li>
+                    <p>151/1 Andagalawaththa Bamunugedara Kurunegala</p>
+                    <li>Phone Number</li>
+                    <p>076 5858504</p>
+                    <li>Email</li>
+                    <p>keshankulasekara@gmail.com</p>
+                </div>
+            </div>
+            <div className="mainright"></div>
+        </div>
         </>
     )
 }
