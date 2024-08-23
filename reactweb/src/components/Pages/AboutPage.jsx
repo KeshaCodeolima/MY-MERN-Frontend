4a79bf07ed46cd756b1f11a15fe2d7c1fe5b37cd
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2024-08-23 10:53:49 +0530

    Add new Auto slider using the npm package

--- reactweb/src/components/Pages/AboutPage.jsx
+++ reactweb/src/components/Pages/AboutPage.jsx
@@ -1,29 +1,31 @@
-import {useState,useEffect} from "react";
+import React from "react";
 import './AboutPage.css';
+import { Zoom } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css';
+import Image1 from '../../Image/IBG5.jpg';
+import Image2 from '../../Image/IBG6.jpg';
+import Image3 from '../../Image/IBG7.jpg';
 
 function AboutPage(){
 
-    const images = [
-        require('../../Image/IBG5.jpg'),
-        require('../../Image/IBG6.jpg'),
-        require('../../Image/IBG7.jpg')
-      ];
+    const images = [Image1,Image2,Image3];
 
-      const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 6000);
-        return () => clearInterval(interval);
-    }, [images.length]);
+    const zoomOutProperties = {
+        duration: 6000,
+        transitionDuration: 500,
+        infinite: true,
+        scale: 0.6,
+        arrows: true
+      };
 
     return(
         <>
-        <div className="slider">
-            <div className="boxborder">
-                <img src={images[currentIndex]} alt="slider" className="slider-image" />
-            </div>
+        <div className="slide-container">
+            <Zoom {...zoomOutProperties}>
+                {
+                    images.map((each,index)=><img key={index} style={{width:"100%",height:"66vh",borderRadius:"20px"}} src={each} alt="Picter"/>)
+                }
+            </Zoom>
         </div>
         <div className="abouttext">
             <div className="aboutboxtext">
